d0561d4964d61035b114238fa2a7c5eb0473e76f
Minkook <minguk2425@gmail.com>
2025-10-30 17:45:52 +0900

    로그인/로그아웃 기능 구현

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createContext } from "react";
+
+const AppContext = createContext()
+
+export const AppContextProvider = (props) =>{
+
+    const value = {
+
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
---
+++ client/src/pages/student/CoursesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoursesList = () => {
+  return (
+    <div>CourseList</div>
+  )
+}
+
+export default CoursesList
